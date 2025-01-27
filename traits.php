345f985c033e035d43f7651427f2c9eeaf257a3c
yugalcap <yugal.shrivastavap@gmail.com>
2025-01-27 15:24:22 +0530

    yugal 1

---
+++ traits.php
@@ -0,0 +1,35 @@
+<?php 
+/* tarits use on simple*/
+trait Stu
+{
+public function demo(){
+	echo "hello yugal";
+}
+}
+class base
+{
+	use Stu;
+}
+$base =new base;
+echo $base->demo();
+/* close tarits use on simple*/
+trait Stu
+{
+public function demo(){
+	echo "hello yugal";
+}
+}
+trait Mang
+{
+public function demo(){
+	echo "welcome yugal";
+}
+}
+class base
+{
+	use Stu;
+}
+$base =new base;
+echo $base->demo();
+
+?>
